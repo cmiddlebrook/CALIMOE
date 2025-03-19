3364cb4c1cf9fccd321d75834ba23ad98df1c6a3
Caroline Middlebrook <caroline.middlebrook@googlemail.com>
2025-03-19 13:39:48 +0000

    testing git...

--- Calimoe.cs
+++ Calimoe.cs
@@ -18,7 +18,7 @@ public class Calimoe : Game
     protected TimeSpan _fpsTimer;
     protected int _fps = 0;
     protected TextObject _fpsFont;
-    protected bool _showFPS = false;
+    protected bool _showFPS = true;
 
     public Color ClearColour {  get; set; }
 
